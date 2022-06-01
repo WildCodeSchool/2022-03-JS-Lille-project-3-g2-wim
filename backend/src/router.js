ab6e07d5a13933a2d8c52611d99cfb92048dc8b2
Thomas Di Cola <100589313+ThomasDCW@users.noreply.github.com>
2022-06-01 13:57:57 +0200

    fix syntax

--- backend/src/router.js
+++ backend/src/router.js
@@ -5,7 +5,6 @@ const { ItemController, LessonController } = require("./controllers");
 const router = express.Router();
 
 router.get("/lessons", LessonController.browse);
-
 router.get("/items", ItemController.browse);
 router.get("/items/:id", ItemController.read);
 router.put("/items/:id", ItemController.edit);
