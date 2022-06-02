64fac445c29f84a03e47da854edaa2374c5f05a9
remi59800 <99273318+remi59800@users.noreply.github.com>
2022-06-02 11:48:03 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import LogIn from "./pages/LogIn";
-import SignIn from "./pages/SignIn";
-import Subjects from "./pages/Subjects";
-import Stream from "./pages/Stream";
+import Header from "@components/Header";
+import Home from "@pages/Home";
+import LogIn from "@pages/LogIn";
+import SignIn from "@pages/SignIn";
+import Subjects from "@pages/Subjects";
+import Stream from "@pages/Stream";
 
 import Styled from "./AppStyle";
 
