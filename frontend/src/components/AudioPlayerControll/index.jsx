8a9ef9b9ef44ed5a708da76a515de7da73df64f8
juliane <jcasier@protonmail.com>
2022-06-07 15:04:51 +0200

    Putting a *.mp3 / *.mp4 ... in gitignore and using dotenv in frontend to easily change url with a VITE_BACKEND_URL

--- frontend/src/components/AudioPlayerControll/index.jsx
+++ frontend/src/components/AudioPlayerControll/index.jsx
@@ -8,10 +8,11 @@ export default function AudioPlayerControll() {
   const [playOn, setPlayOn] = useState(false);
   const [audio, setAudio] = useState(null);
   const [playOrPauseImg, setPlayOrPauseImg] = useState(playImg);
-
   useEffect(() => {
     setAudio(
-      new Audio("http://localhost:5000/assets/espagnol5SePresenter.mp3")
+      new Audio(
+        `${import.meta.env.VITE_BACKEND_URL}/assets/espagnol5SePresenter.mp3`
+      )
     );
   }, []);
 
