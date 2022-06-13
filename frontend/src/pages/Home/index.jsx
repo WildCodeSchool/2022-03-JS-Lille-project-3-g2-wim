96773a3b426c8a2ee87a50b4dfa68e1fa7dd768c
n-faycel <faycel.nasmi@outlook.fr>
2022-06-13 10:27:25 +0200

    creation pages home and caroussel

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,13 +1,54 @@
+// eslint-disable-next-line import/no-unresolved
+import { Swiper, SwiperSlide } from "swiper/react";
+// eslint-disable-next-line import/no-unresolved
+import "swiper/css";
+// eslint-disable-next-line import/no-unresolved
+import "swiper/css/navigation";
+// eslint-disable-next-line import/no-unresolved
+import "swiper/css/pagination";
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 import SHome from "./style";
 
-function Home() {
+export default function Home() {
+  const images = [
+    {
+      destop: "../../../src/assets/home-Logo.png",
+      mobile: "../../../src/assets/homeLogo.jpg",
+    },
+    {
+      destop: "../../../src/assets/home2.png",
+      mobile: "../../../src/assets/home2.jpg",
+    },
+    {
+      destop: "../../../src/assets/home-3.png",
+      mobile: "../../../src/assets/home3.jpg",
+    },
+    {
+      destop: "../../../src/assets/home-4.png",
+      mobile: "../../../src/assets/home4.jpg",
+    },
+  ];
+
   return (
     <SHome>
-      <div>
-        <p>Home</p>
-      </div>
+      <Swiper
+        cssMode
+        navigation
+        pagination
+        mousewheel
+        keyboard
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper"
+      >
+        {images.map((image) => (
+          <SwiperSlide>
+            <picture>
+              <source srcSet={image.mobile} media="(max-width: 700px)" />
+              <img src={image.destop} alt="home" />
+            </picture>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </SHome>
   );
 }
-
-export default Home;
