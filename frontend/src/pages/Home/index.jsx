a7ac60a8da275b815d67a5bbf724a4dc159a39a0
juliane <jcasier@protonmail.com>
2022-06-13 15:38:55 +0200

    [200~

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -3,10 +3,25 @@ import Navbar from "@components/Navbar";
 import SHome from "./style";
 
 function Home() {
+  const topics = [
+    { id: 1, name: "Philosophie", img: "" },
+    { id: 2, name: "Histoire Géographie", img: "" },
+    { id: 3, name: "Science de la Vie et de la Terre", img: "" },
+    { id: 4, name: "Physique Chimie", img: "" },
+    { id: 5, name: "Mathématiques", img: "" },
+    { id: 6, name: "Espagnol", img: "" },
+  ];
   return (
     <SHome>
       <Header />
-      <p>Home</p>
+      <>
+        <h1>Matières</h1>
+        <ul>
+          {topics.map((topic) => (
+            <li key={topic.id}>{topic.name}</li>
+          ))}
+        </ul>
+      </>
       <Navbar />
     </SHome>
   );
