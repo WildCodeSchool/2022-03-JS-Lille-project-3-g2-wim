5871119fcc1e4f879ae9d98c2abfe9a68ffc6f60
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-15 09:39:20 +0200

    fix stream css

--- frontend/src/pages/Stream/style.jsx
+++ frontend/src/pages/Stream/style.jsx
@@ -1,22 +1,21 @@
 import styled from "styled-components";
 
 export default styled.section`
-  min-height: 100vh;
   .containStream {
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 50vh;
+    height: 40vh;
 
     .circle {
-      width: 30rem;
-      height: 30rem;
+      width: 16rem;
+      height: 16rem;
       border-radius: 50%;
       border: 1px var(--colorBlueMain) solid;
       box-shadow: 0px 0px 40px 10px #77777737;
     }
     img {
-      width: 20rem;
+      width: 12rem;
       position: absolute;
     }
   }
@@ -36,12 +35,23 @@ export default styled.section`
   @media screen and (max-width: 500px) {
     .containStream {
       .circle {
-        width: 22rem;
-        height: 22rem;
+        width: 16rem;
+        height: 16rem;
       }
       img {
-        height: 15rem;
-        width: 15rem;
+        height: 12rem;
+      }
+    }
+    .lessonTopic {
+      text-align: center;
+      .lesson {
+        color: #2999ff;
+        font-size: 1.2rem;
+        margin-top: 2rem;
+      }
+      .topic {
+        font-size: 1.2rem;
+        margin-top: 0.8rem;
       }
     }
   }
