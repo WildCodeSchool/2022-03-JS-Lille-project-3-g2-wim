631bca876f344c00e0f243e65652edf2be863541
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-15 10:58:04 +0200

    create authcontroller.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,14 +1,32 @@
 const express = require("express");
+const passport = require("passport");
 
-const { ItemController, LessonController } = require("./controllers");
+const {
+  ItemController,
+  LessonController,
+  AuthController,
+} = require("./controllers");
 
 const router = express.Router();
 
+// get routes /lessons
+
 router.get("/lessons", LessonController.browse);
+
+//  auth routes
+
+router.post("/auth/signup", AuthController.signup);
+router.post(
+  "/auth/login",
+  passport.authenticate("local"),
+  AuthController.login
+);
+
+// items routes by default
+
 router.get("/items", ItemController.browse);
 router.get("/items/:id", ItemController.read);
 router.put("/items/:id", ItemController.edit);
 router.post("/items", ItemController.add);
 router.delete("/items/:id", ItemController.delete);
-
 module.exports = router;
