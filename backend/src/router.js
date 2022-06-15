bcaefbc71c92498b5299b2635afd4d043bf62a7c
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-15 17:00:02 +0200

    authentification with token and bcrypt

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,15 +1,35 @@
 const express = require("express");
+const passport = require("passport");
 
-const { ItemController, LessonController } = require("./controllers");
+const {
+  ItemController,
+  LessonController,
+  AuthController,
+} = require("./controllers");
 
 const router = express.Router();
 
+// get routes /lessons
 router.get("/lessons", LessonController.browse);
-router.get("/lessons/:id", LessonController.read);
+
+//  auth routes
+router.post("/auth/signup", AuthController.signup);
+router.post(
+  "/auth/login",
+  passport.authenticate("local", { session: false }),
+  AuthController.login
+);
+
+router.get(
+  "/secure/lessons",
+  passport.authenticate("jwt", { session: false }),
+  AuthController.lessons
+);
+
+// items routes by default
 router.get("/items", ItemController.browse);
 router.get("/items/:id", ItemController.read);
 router.put("/items/:id", ItemController.edit);
 router.post("/items", ItemController.add);
 router.delete("/items/:id", ItemController.delete);
-
 module.exports = router;
