48dfb5d3919ebf7e67542782902d74f985f19f92
juliane <jcasier@protonmail.com>
2022-06-16 14:48:38 +0200

    Modifying carousel to put a link to the last image

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -4,26 +4,36 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 /* eslint-enable */
+import { Link } from "react-router-dom";
 import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 import SHome from "./style";
 
 function Home() {
-  const images = [
+  const imagesIntro = [
     {
+      key: "logo",
       desktop: "../../../src/assets/Images/home-Logo.png",
       mobile: "../../../src/assets/Images/homeLogo.jpg",
+      alt: "logo de WIM",
     },
     {
+      key: "image1",
       desktop: "../../../src/assets/Images/home2.png",
       mobile: "../../../src/assets/Images/home1.jpg",
+      alt: "Dans un univers de paix et d'amour, la musique serait le langage universel",
     },
     {
+      key: "image2",
       desktop: "../../../src/assets/Images/home-3.png",
       mobile: "../../../src/assets/Images/home2.jpg",
+      alt: "La musique peut rendre les gens meilleurs, il suffit de la leur injecter constamment",
     },
     {
+      key: "image3",
       desktop: "../../../src/assets/Images/home-4.png",
       mobile: "../../../src/assets/Images/home3.jpg",
+      alt: "Chaque enfant qu'on enseigne est un Homme qu'on gagne",
+      url: "/Subjects",
     },
   ];
   return (
@@ -37,12 +47,24 @@ function Home() {
         modules={[Navigation, Pagination, Mousewheel, Keyboard]}
         className="mySwiper"
       >
-        {images.map((image) => (
+        {imagesIntro.map((imageIntro) => (
           <SwiperSlide>
-            <picture>
-              <source srcSet={image.mobile} media="(max-width: 700px)" />
-              <img src={image.desktop} alt="home" />
-            </picture>
+            {imageIntro.key === "image3" ? (
+              <Link to={imageIntro.url} key={imageIntro.key}>
+                <picture>
+                  <source
+                    srcSet={imageIntro.mobile}
+                    media="(max-width: 700px)"
+                  />
+                  <img src={imageIntro.desktop} alt={imageIntro.alt} />
+                </picture>
+              </Link>
+            ) : (
+              <picture key={imageIntro.key}>
+                <source srcSet={imageIntro.mobile} media="(max-width: 700px)" />
+                <img src={imageIntro.desktop} alt={imageIntro.alt} />
+              </picture>
+            )}
           </SwiperSlide>
         ))}
       </Swiper>
