2befb70d9b95d430001a71ab11fd117c84fc874e
Dash293 <100704568+Dash293@users.noreply.github.com>
2022-06-17 16:19:22 +0200

    Update index.jsx

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 import SHome from "./style";
 
-function Home() {
+export default function Home() {
   const imagesIntro = [
     {
       key: "logo",
