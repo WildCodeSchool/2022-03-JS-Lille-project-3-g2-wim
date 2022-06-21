3340d191249d2d921b248e04315c7cbd3d383c7d
juliane <jcasier@protonmail.com>
2022-06-21 15:52:32 +0200

    Decomposing the LessonCard to make it manage only one lesson and putting axios call in lesson page

--- frontend/src/components/LessonCard/index.jsx
+++ frontend/src/components/LessonCard/index.jsx
@@ -1,30 +1,28 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
+import propTypes from "prop-types";
 import SLessonCard from "./style";
 import Play from "../../assets/play-solid.svg";
 import EmptyHeart from "../../assets/Images/emptyheart.svg";
 
-export default function LessonCard() {
-  const [lessons, setLessons] = useState([]);
-  useEffect(() => {
-    axios.get(`${import.meta.env.VITE_BACKEND_URL}${"/lessons"}`).then(({ data }) => {
-      setLessons(data);
-    });
-  }, []);
+export default function LessonCard({ key, title, duration, musicStyle }) {
   return (
     <SLessonCard>
-      <>
-        {lessons.map((lesson) => (
-          <div key={lesson.id} className="CardOfLesson">
-            <img className="play" src={Play} alt="play button" />
-            <div className="informations">
-              <h1 className="lesson"> {lesson.title}</h1>
-              <h2>Hip-Hop/{lesson.duration}</h2>
-            </div>
-            <img className="heart" src={EmptyHeart} alt="add favourite button" />
-          </div>
-        ))}
-      </>
+      <div key={key} className="CardOfLesson">
+        <img className="play" src={Play} alt="play button" />
+        <div className="informations">
+          <h1 className="lesson"> {title}</h1>
+          <h2>
+            {musicStyle} / {duration}
+          </h2>
+        </div>
+        <img className="heart" src={EmptyHeart} alt="add favourite button" />
+      </div>
     </SLessonCard>
   );
 }
+
+LessonCard.propTypes = {
+  key: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+  duration: propTypes.number.isRequired,
+  musicStyle: propTypes.string.isRequired,
+};
--- frontend/src/pages/Lessons/index.jsx
+++ frontend/src/pages/Lessons/index.jsx
@@ -1,13 +1,31 @@
 import Header from "@components/Header";
 import LessonCard from "@components/LessonCard";
 import Navbar from "@components/Navbar";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import SLessons from "./style";
 
 function Lessons() {
+  const [lessons, setLessons] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}${"/lessons"}`)
+      .then(({ data }) => {
+        setLessons(data);
+      });
+  }, []);
   return (
     <SLessons>
       <Header />
-      <LessonCard />
+      {lessons.map((lesson) => (
+        <LessonCard
+          key={lesson.id}
+          title={lesson.title}
+          duration={lesson.duration}
+          musicStyle={lesson.musicStyle}
+        />
+      ))}
+
       <Navbar />
     </SLessons>
   );
