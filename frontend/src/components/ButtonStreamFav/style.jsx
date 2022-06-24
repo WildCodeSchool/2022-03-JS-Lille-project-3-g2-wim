8d0ad91de9e244e57b6f355575df7e9a64661587
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-24 16:26:21 +0200

    fixing button responsive

--- frontend/src/components/ButtonStreamFav/style.jsx
+++ frontend/src/components/ButtonStreamFav/style.jsx
@@ -3,11 +3,10 @@ import styled from "styled-components";
 export default styled.div`
   margin: 2rem 0;
   button {
-    width: 6rem;
-    height: 6rem;
+    width: 4rem;
+    height: 4rem;
     background: no-repeat url(${(props) => props.imgFav});
     background-size: contain;
-    background-size: 4vw;
     border: white solid 1px;
     margin: 0 1rem;
   }
