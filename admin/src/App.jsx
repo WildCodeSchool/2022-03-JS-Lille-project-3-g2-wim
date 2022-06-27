1fc87350d21665ea8ec34fb40f8c7a73ce365999
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-27 10:24:18 +0200

    add Listguesser for users and lessons

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,14 @@
-import { Admin, Resource } from "react-admin";
+import { Admin, Resource, ListGuesser } from "react-admin";
+
+import dataProvider from "./dataProvider";
+import authProvider from "./authProvider";
 
 function App() {
   return (
     <div className="App">
-      <Admin>
-        <Resource />
+      <Admin dataProvider={dataProvider} authProvider={authProvider}>
+        <Resource name="users" list={ListGuesser} />
+        <Resource name="lessons" list={ListGuesser} />
       </Admin>
     </div>
   );
