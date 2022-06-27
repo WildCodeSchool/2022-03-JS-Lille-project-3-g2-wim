3a945f73621d5944f9d8b032f12a6d82b074f3ee
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-27 10:25:07 +0200

    fix cores

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,21 @@ const router = require("./router");
 require("./passport-strategies");
 
 const app = express();
+const corsWhitelist = [process.env.FRONTEND_URL, process.env.ADMIN_URL];
 
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (corsWhitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 // use some application-level middlewares
 app.use(
   cors({
