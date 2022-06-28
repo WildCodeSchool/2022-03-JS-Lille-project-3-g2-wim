8931f24e7bc774a1f34101708ac7728ed45d3ccd
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-28 10:50:49 +0200

    move the services folder and  fix cores

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,12 +22,6 @@ app.use(
   })
 );
 // use some application-level middlewares
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
-    optionsSuccessStatus: 200,
-  })
-);
 
 app.use(express.json());
 
