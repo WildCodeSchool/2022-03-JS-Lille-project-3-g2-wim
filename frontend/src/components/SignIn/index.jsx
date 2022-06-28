ce8b174062f0d2038e1ada0043e3b31260e108e6
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-28 11:33:26 +0200

    signin work

--- frontend/src/components/SignIn/index.jsx
+++ frontend/src/components/SignIn/index.jsx
@@ -1,31 +1,55 @@
 import { Button, TextField, Box } from "@mui/material";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 import SSignIn from "./style";
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const hSubmit = (evt) => {
+    evt.preventDefault();
+    axios
+      .post("http://localhost:5000/auth/login", formData)
+      .then(() => {
+        toast("Wow so easy!");
+      })
+      .catch(() => {
+        toast.error("Aww, it broke!");
+      });
+  };
+  const hChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormData({ ...formData, [name]: value });
+  };
   return (
-    <SSignIn>
+    <SSignIn action="" onSubmit={hSubmit}>
       <Box>
         <TextField
           required
-          id="Email"
-          name="Email"
-          label="Email"
+          id="email"
+          name="email"
+          label="email"
           fullWidth
           variant="standard"
+          value={formData.email}
+          onChange={hChange}
         />
         <TextField
           type="password"
           required
           id="Mot de passe"
-          name="Mot de passe"
+          name="password"
           label="Confirmer le mot de passe"
           fullWidth
           variant="standard"
+          value={formData.password}
+          onChange={hChange}
         />
       </Box>
       <Button variant="contained" size="large">
-        Se connecter
+        Connecter
       </Button>
+      <input className="submit" type="submit" value="ENVOYER" />
     </SSignIn>
   );
 }
--- frontend/src/components/SignIn/style.jsx
+++ frontend/src/components/SignIn/style.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export default styled.div`
+export default styled.form`
   display: flex;
   justify-content: center;
   flex-direction: column;
