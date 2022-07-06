fe4113d7ee5f4976c16c012ac845a30050d71d32
ThomasDCW <thomas.dicola@hotmail.fr>
2022-07-06 10:26:19 +0200

    add icon user

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,14 +5,21 @@ import LessonEdit from "@components/Lessons/lessonEdit";
 import LessonList from "@components/Lessons/lessonList";
 import TopicList from "@components/Topic/topicList";
 import TopicEdit from "@components/Topic/topicEdit";
+import UserIcon from "@mui/icons-material/Group";
+import Dashboard from "./Dashboard";
 
 import dataProvider from "./dataProvider";
 
 function App() {
   return (
     <div className="App">
-      <Admin dataProvider={dataProvider}>
-        <Resource name="users" list={UserList} edit={UserEdit} />
+      <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+        <Resource
+          name="users"
+          list={UserList}
+          edit={UserEdit}
+          icon={UserIcon}
+        />
         <Resource name="lessons" list={LessonList} edit={LessonEdit} />
         <Resource name="topics" list={TopicList} edit={TopicEdit} />
       </Admin>
