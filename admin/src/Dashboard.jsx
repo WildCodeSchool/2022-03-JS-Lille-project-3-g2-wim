18c03d5d49a54ca360055a9979556f527c5ec9b1
ThomasDCW <thomas.dicola@hotmail.fr>
2022-07-06 14:47:14 +0200

    fix dashboard

--- admin/src/Dashboard.jsx
+++ admin/src/Dashboard.jsx
@@ -1,15 +1,18 @@
-import { Card, CardContent, CardHeader } from "@mui/material";
+import { Card, CardContent, CardHeader, Typography } from "@mui/material";
 
 export default function DashBoard() {
   return (
     <Card>
       <CardHeader title="Bienvenue sur l'administration de WIM" />
       <CardContent>
-        Votre espace administrable vous permet de : <br />
-        Consulter, modifier les utilisateurs.
+        <Typography>Votre espace administrable vous permet de :</Typography>
         <br />
-        Ajouter, modifier, supprimer les cours. <br />
-        Ajouter, modifier, supprimer les matières.
+        <Typography>
+          Consulter, modifier les utilisateurs.
+          <br />
+          Ajouter, modifier, supprimer les cours. <br />
+          Ajouter, modifier, supprimer les matières.
+        </Typography>
       </CardContent>
     </Card>
   );
