53093eed10a05ba38030877fc5c8ba506fb6ae91
ThomasDCW <thomas.dicola@hotmail.fr>
2022-07-08 11:55:24 +0200

    update user administration

--- admin/src/components/Users/userEdit.jsx
+++ admin/src/components/Users/userEdit.jsx
@@ -1,29 +1,16 @@
-import {
-  Edit,
-  NumberInput,
-  ReferenceInput,
-  SelectInput,
-  SimpleForm,
-  TextInput,
-} from "react-admin";
+import { Edit, NumberInput, SimpleForm, TextInput } from "react-admin";
 
 export default function UserEdit() {
   return (
     <Edit>
       <SimpleForm>
-        <TextInput source="id" />
         <TextInput source="email" />
         <TextInput source="name" />
         <TextInput source="firstname" />
         <NumberInput source="age" />
-        <TextInput source="phoneNumber" />
         <TextInput source="schoolOption" />
         <TextInput source="schoolName" />
-        <TextInput source="country" />
-        <TextInput source="city" />
-        <ReferenceInput source="schoolClass_id" reference="users">
-          <SelectInput optionText="id" />
-        </ReferenceInput>
+        <TextInput source="schoolClass_id" />
       </SimpleForm>
     </Edit>
   );
--- admin/src/components/Users/userList.jsx
+++ admin/src/components/Users/userList.jsx
@@ -3,7 +3,6 @@ import {
   EmailField,
   List,
   NumberField,
-  ReferenceField,
   TextField,
 } from "react-admin";
 
@@ -11,7 +10,6 @@ export default function UserList() {
   return (
     <List>
       <Datagrid rowClick="edit">
-        <TextField source="id" />
         <EmailField source="email" />
         <TextField source="name" />
         <TextField source="firstname" />
@@ -19,11 +17,7 @@ export default function UserList() {
         <TextField source="phoneNumber" />
         <TextField source="schoolOption" />
         <TextField source="schoolName" />
-        <TextField source="country" />
-        <TextField source="city" />
-        <ReferenceField source="schoolClass_id" reference="users">
-          <TextField source="schoolClass_id" />
-        </ReferenceField>
+        <TextField source="schoolClass_id" />
       </Datagrid>
     </List>
   );
