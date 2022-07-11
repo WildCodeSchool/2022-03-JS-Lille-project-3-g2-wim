0f2e8d0a9f8e21f813777f8810b4b8e4a2d3b7cf
ThomasDCW <thomas.dicola@hotmail.fr>
2022-07-11 11:04:03 +0200

    add route, manager, controller for classes

--- backend/src/models/ClassManager.js
+++ backend/src/models/ClassManager.js
@@ -1,7 +1,21 @@
 const AbstractManager = require("./AbstractManager");
 
-class ItemManager extends AbstractManager {
+class ClassManager extends AbstractManager {
   static table = "schoolClass";
+
+  insert(item) {
+    return this.connection.query(
+      `insert into ${ClassManager.table} (title) values (?)`,
+      [item.title]
+    );
+  }
+
+  update(item) {
+    return this.connection.query(
+      `update ${ClassManager.table} set title = ? where id = ?`,
+      [item.title, item.id]
+    );
+  }
 }
 
-module.exports = ItemManager;
+module.exports = ClassManager;
