a7abf6015c7155364db25707af0251f6652dbbd8
Amar <amar@uub.home>
2022-07-18 15:30:54 +0200

    fix dev header

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,19 +1,33 @@
-import avatar from "@assets/avatar.png";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import Avatar from "@components/Avatar";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import SHeader from "./style";
 
 function Header() {
-  const { id } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const { email } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const deconnect = () => {
+    dispatch({ type: "USER_LOGOUT" });
+    navigate("/connexion");
+  };
 
   return (
     <SHeader>
-      <Link to={`/profil/${id}`}>
-        <img src={avatar} alt="avatar" />
+      <Link to="/profil/:id">
+        <img className="avatar" src={Avatar} alt="avatar" />
       </Link>
       <Link to="/accueil">
         <p>WIM</p>
       </Link>
+      {email && (
+        <input
+          type="button"
+          onClick={deconnect}
+          className="deconnect"
+          value="Deconnexion"
+        />
+      )}
     </SHeader>
   );
 }
