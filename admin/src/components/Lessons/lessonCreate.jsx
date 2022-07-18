7ada55c18f833d6a67c91fee1148aef040e941d4
ThomasDCW <thomas.dicola@hotmail.fr>
2022-07-18 09:47:14 +0200

    i taostify

--- admin/src/components/Lessons/lessonCreate.jsx
+++ admin/src/components/Lessons/lessonCreate.jsx
@@ -1,6 +1,24 @@
 import { Create, NumberInput, SimpleForm, TextInput } from "react-admin";
+import { useState } from "react";
 
 export default function LessonCreate() {
+  const [formData, setFormData] = useState({});
+
+  const hChange = (evt) => {
+    const { name, value, type, checked, files } = evt.target;
+    let newValue = null;
+    switch (type) {
+      case "checkbox":
+        newValue = checked;
+        break;
+      case "file":
+        [newValue] = files;
+        break;
+      default:
+        newValue = value;
+    }
+    setFormData({ ...formData, [name]: newValue });
+  };
   return (
     <Create>
       <SimpleForm>
@@ -13,7 +31,10 @@ export default function LessonCreate() {
         />
         <TextInput label="Titre" source="title" />
         <TextInput label="Genre" source="musicStyle" />
-        <TextInput label="Emplacement" source="fileLocation" />
+        {/* <TextInput label="Emplacement" source="fileLocation" /> */}
+        <form method="post" encType="multipart/form-data" onSubmit={hSubmit}>
+          <input type="file" onChange={hChange} />
+        </form>
         <NumberInput min="0" label="Durée" source="duration" />
         <NumberInput
           min="1"
