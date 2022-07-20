3e773cf7c2e556ed54565707599d6c5914684725
Jeremy <jeremy.jorand@gmail.com>
2022-07-20 07:34:41 +0200

    creation of a regex in the backend and modification of the registration form as well as the correct functioning of the avatar

--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -6,30 +6,35 @@ const { uploadImage } = require("../services/cloudinary");
 class AuthController {
   static signup = async (req, res) => {
     const user = req.body;
+    const userEmail = user.email;
+    const regexEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
 
     // TODO validations (length, format...)
     user.password = bcrypt.hashSync(
       user.password,
       parseInt(process.env.CRYPT_ROUNDS, 10)
     );
-    if (user.email !== /[\w_-]+@[\w-]+\.[a-z]{2,3}$/i) {
-      res.status(418).res.send("Email incorrect");
+    if (user.avatar !== "") {
+      const feedbackImg = await uploadImage(req.file.path);
+      user.avatar = feedbackImg.secure_url;
     }
-    const feedbackImg = await uploadImage(req.file.path);
-    user.avatar = feedbackImg.secure_url;
 
-    models.user
-      .insert(user)
-      .then(([result]) => {
-        user.id = result.insertId;
-        delete user.password;
-        const token = jwt.sign(user, process.env.JWT_SECRET);
-        res.status(201).send({ user, token });
-      })
-      .catch((err) => {
-        console.error(err);
-        res.sendStatus(500);
-      });
+    if (userEmail.match(regexEmail)) {
+      models.user
+        .insert(user)
+        .then(([result]) => {
+          user.id = result.insertId;
+          delete user.password;
+          const token = jwt.sign(user, process.env.JWT_SECRET);
+          res.status(201).send({ user, token });
+        })
+        .catch((err) => {
+          console.error(err);
+          res.sendStatus(500);
+        });
+    } else {
+      res.status(400).send("Email incorrect");
+    }
   };
 
   static login = (req, res) => {
