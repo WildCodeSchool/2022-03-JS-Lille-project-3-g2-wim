0a1ca48e66b9aeb02bfbabc77314b47246faa9d0
remi59800 <remimenart@gmail.com>
2022-07-25 14:51:53 +0200

    bugs-on-topics-page-mobile-fixed

--- frontend/src/components/ButtonStreamFav/style.jsx
+++ frontend/src/components/ButtonStreamFav/style.jsx
@@ -1,13 +1,11 @@
 import styled from "styled-components";
 
 export default styled.div`
-  margin: 2rem 0;
   button {
     width: 4rem;
     height: 4rem;
     background: no-repeat url(${(props) => props.imgFav});
     background-size: contain;
     border: white solid 1px;
-    margin: 0 1rem;
   }
 `;
