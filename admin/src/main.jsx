2661014e7faef5c4cf0f6442bce46a4f308159ff
ThomasDCW <thomas.dicola@hotmail.fr>
2022-07-25 11:44:43 +0200

    fix pacakage and delete toast in man.jsx

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ToastContainer />
     <App />
   </React.StrictMode>
 );
