05c28b1186c549de90f28344153258423ae0fa62
Jeremy <jeremy.jorand@gmail.com>
2022-07-27 10:47:51 +0200

    fix button audio

--- frontend/src/components/AudioButton/style.jsx
+++ frontend/src/components/AudioButton/style.jsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 
 export default styled.div`
+  display: flex;
+  align-items: center;
+  height: 5rem;
   button {
     width: 6vw;
     height: 4vh;
     background: no-repeat url(${(props) => props.img});
+    background-position: center;
     background-size: contain;
     border: none;
     margin: 1.4em;
